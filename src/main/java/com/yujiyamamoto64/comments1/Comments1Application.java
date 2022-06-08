9ac24915266a1026475b0e4b36e6314d2d4e3e5f
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-06-08 10:05:15 -0300

    First commit - project created

---
+++ src/main/java/com/yujiyamamoto64/comments1/Comments1Application.java
@@ -0,0 +1,13 @@
+package com.yujiyamamoto64.comments1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comments1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comments1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/yujiyamamoto64/comments1/Comments1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yujiyamamoto64.comments1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comments1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
