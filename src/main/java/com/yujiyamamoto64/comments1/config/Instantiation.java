b0e04d49f1638d37ea324c12a3ff922462a59b50
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-06-10 11:32:14 -0300

    Author data with DTO projection

--- src/main/java/com/yujiyamamoto64/comments1/config/Instantiation.java
+++ src/main/java/com/yujiyamamoto64/comments1/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.yujiyamamoto64.comments1.domain.Post;
 import com.yujiyamamoto64.comments1.domain.User;
+import com.yujiyamamoto64.comments1.dto.AuthorDTO;
 import com.yujiyamamoto64.comments1.repositories.PostRepository;
 import com.yujiyamamoto64.comments1.repositories.UserRepository;
 
@@ -35,10 +36,12 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para SP. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para SP. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
